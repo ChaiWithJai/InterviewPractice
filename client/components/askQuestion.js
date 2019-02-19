11372ab2b80f927432f1ab9831d1c59038adc0e1
Jay Bhagat <user.email>
2019-02-19 14:03:05 -0500

    askQuestion component made and boiler set-up for React component and testing url within ReactRouter

---
+++ client/components/askQuestion.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class askQuestion extends Component {
+  render() {
+    return (
+      <div>
+        <p>Questions are asked here</p>
+      </div>
+    )
+  }
+}
+
+export default askQuestion
